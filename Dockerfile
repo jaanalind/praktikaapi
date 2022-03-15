8e0d5657f22d54d915cd88bc0b7dc8139135df06
jaanalind <kaspar.jesmin@gmail.com>
2022-03-15 02:26:43 +0200

    some testing

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM postgres:latest
+FROM python:alpine
 RUN apt-get update && apt-get install -y python3 python3-pip
 RUN apt-get -y install python3.7-dev
 RUN apt-get install
--- scraper.py
+++ scraper.py
@@ -1,11 +1,45 @@
 import requests
 from time import time
+#!/usr/bin/python
+import psycopg2
 
+try:
+    conn = psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        password="dbadmin")
+except:
+    print("Unable to connect")
 
-URL = "https://dashboard.elering.ee/api/nps/price"
+cur = conn.cursor()
+
+URL = "https://dashboard.elering.ee/api/system"
+
 response = requests.get(URL)
 data = response.json()
-print(data['data']['ee'])
+i=13
+for row in data["data"]:
+    if i==13:
+        cur.execute("""INSERT INTO elering_data (production, consumption, price)
+        #VALUES (row['production'], 3.54, 4.234);""")
+        i=1
+    i+=1
+
+conn.commit()
+cur.close()
+conn.close()
+
+
+
+
+#URL = "https://dashboard.elering.ee/api/system?start=2021-03-15T20%3A59%3A59.999Z&end=2022-03-15T20%3A59%3A59.999Z"
+
+#print(data['data'])
+#URL = "https://dashboard.elering.ee/api/nps/price"
+#response = requests.get(URL)
+#data = response.json()
+#print(data['data']['ee'])
 
 
 
