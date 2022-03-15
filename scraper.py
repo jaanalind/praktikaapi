5dc42ee15aeb757d0f0b554d5d977401ee422568
jaanalind <kaspar.jesmin@gmail.com>
2022-03-16 00:26:23 +0200

    working db

--- scraper.py
+++ scraper.py
@@ -3,43 +3,71 @@ from time import time
 #!/usr/bin/python
 import psycopg2
 
-try:
-    conn = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user="postgres",
-        password="dbadmin")
-except:
-    print("Unable to connect")
 
-cur = conn.cursor()
 
-URL = "https://dashboard.elering.ee/api/system"
+class DBData:
+    def __init__(self, URL):
+        self.URL = URL
+    
+    def dataAsDict(self):
+        try:
+            response = requests.get(self.URL)
+            data = response.json()
+            return data
+        except:
+            raise Exception("Can't get data from: "+self.URL)
+    
+    def systemData(self):
+        data = self.dataAsDict()
+        data = data['data'][::12]
+        return data
+    
+    def priceData(self):
+        data = self.dataAsDict()
+        data = data['data']['ee']
+        return data
 
-response = requests.get(URL)
-data = response.json()
-i=13
-for row in data["data"]:
-    if i==13:
-        cur.execute("""INSERT INTO elering_data (production, consumption, price)
-        #VALUES (row['production'], 3.54, 4.234);""")
-        i=1
-    i+=1
 
-conn.commit()
-cur.close()
-conn.close()
+class dataDictToDB:
+    def __init__(self, dataDict):
+        self.dataDict = dataDict
+    def connectToDB(self):
+        try:
+            conn = psycopg2.connect(
+                host="localhost",
+                database="postgres",
+                user="postgres",
+                password="dbadmin")
+            return conn
+        except:
+            raise Exception("Unable to connect")
+            
+    
+    def dataToDB(self):
+        conn = self.connectToDB()
+        cur = conn.cursor()
+        
+        for values in self.dataDict:
+           #thought about using a tuple, so i could also insert price data first, but if i always to it the same way, i thought it would be pointless 
+            cur.execute("""
+        INSERT INTO elering_data (production, consumption, ts)
+        VALUES (%(production)s, %(consumption)s, to_timestamp(%(ts)s));
+        """,
+        {'production': values['production'], 'consumption': values['consumption'], 'ts':values['timestamp'] })
+        conn.commit()
+        cur.close()
+        conn.close()
 
+systemData = DBData("https://dashboard.elering.ee/api/system")
+systemData = systemData.systemData()
+systemDataToDB = dataDictToDB(systemData)
+systemDataToDB.dataToDB()
 
 
 
 #URL = "https://dashboard.elering.ee/api/system?start=2021-03-15T20%3A59%3A59.999Z&end=2022-03-15T20%3A59%3A59.999Z"
-
-#print(data['data'])
 #URL = "https://dashboard.elering.ee/api/nps/price"
-#response = requests.get(URL)
-#data = response.json()
-#print(data['data']['ee'])
+#https://dashboard.elering.ee/api/system
 
 
 
